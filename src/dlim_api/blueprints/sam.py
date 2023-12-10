c3e46a1341facf72ec9f5daca4fd4795edea45cc
Adam <a.garai98@gmail.com>
2023-12-10 10:45:59 +0000

    loading, save as pil image and return working

--- src/dlim_api/blueprints/sam.py
+++ src/dlim_api/blueprints/sam.py
@@ -19,23 +19,47 @@ logger = logging.getLogger(__name__)
 
 sam_blueprint = Blueprint("sam", __name__)
 
+WORK_DIR = Path().absolute()
+IMAGE_DIR = WORK_DIR / "src/dlim_api/images"
+
+mask_generator = None  # TODO CHANGE to object etc dict wahtever
+
 
 @sam_blueprint.route("/blueprint-hc", methods=["GET"])
 def bp_hc():
-    return str(torch.cuda.is_available())
+    return "BP OK"
 
 
 @sam_blueprint.route("/racoon-in-a-suit", methods=["GET"])
 def send_racoon():
-    WORK_DIR = Path().absolute()  # Gets current working dir
+    # Gets current working dir
     PATH_TO_RACOON = WORK_DIR / "src/dlim_api/images/racoon_in_suit.jpg"
     return send_file(PATH_TO_RACOON, mimetype="image/jpeg")
-    # TODO CHECK if this works with docker, works locally right now
 
 
 @sam_blueprint.route("/sam", methods=["PUT"])
-def send_racoon():
-    WORK_DIR = Path().absolute()  # Gets current working dir
-    PATH_TO_RACOON = WORK_DIR / "src/dlim_api/images/racoon_in_suit.jpg"
-    return send_file(PATH_TO_RACOON, mimetype="image/jpeg")
-    # TODO CHECK if this works with docker, works locally right now
+def load_sam():
+    try:
+        WORK_DIR = Path().absolute()  # Gets current working dir
+        SAM_CHECKPOINT = WORK_DIR / "src/dlim_api/utils/model_checkpoints/sam_vit_h_4b8939.pth"
+        model_type = "vit_h"
+        device = "cuda"
+        sam = sam_model_registry[model_type](checkpoint=SAM_CHECKPOINT)
+        sam.to(device=device)
+
+        mask_generator = SamAutomaticMaskGenerator(model=sam, points_per_batch=32)
+
+        return "Successfuly loaded SAM"
+    except Exception as e:
+        return str(e)
+
+
+@sam_blueprint.route("/sam", methods=["POST"])
+def segment_image():
+    image = request.files["image"]
+    image_name = image.filename
+    # image.save()
+    img = Image.open(image.stream)
+    PATH_TO_IMG = IMAGE_DIR / image_name
+    img.save(PATH_TO_IMG)
+    return send_file(PATH_TO_IMG)
