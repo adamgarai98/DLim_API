aab2b587ca387b4f8dc5df992dcd10f328ac5d5a
Adam <a.garai98@gmail.com>
2023-12-11 19:43:13 +0000

    Initial setup (#9)
    * Skeleton
    
    * Still setup
    
    * Flask Working
    
    Added flask server args parser, default args, simple healthcheck
    
    * Update .gitignore
    
    * Update requirements.txt
    
    Added things needed for SAM, may currently be more than needed
    
    * Update setup.py
    
    minor changes
    
    * formatting
    
    * Create Dockerfile
    
    * import blueprints
    
    * blueprints
    
    * Create __init__.py
    
    * debug
    
    * loading, save as pil image and return working
    
    * segment images
    
    * renaming, restructuring
    
    * Now args are actually parsed...
    
    good to learn how setuptools works
    
    * Moved utils around

--- src/dlim_api/blueprints/sam_bp.py
+++ src/dlim_api/blueprints/sam_bp.py
@@ -14,6 +14,8 @@ from flask import Blueprint, abort, current_app, jsonify, request, send_file
 from PIL import Image
 from segment_anything import SamAutomaticMaskGenerator, SamPredictor, sam_model_registry
 
+from dlim_api.utils.model_utils import sam_utils
+
 logger = logging.getLogger(__name__)
 
 
@@ -21,22 +23,6 @@ sam_blueprint = Blueprint("sam", __name__)
 
 WORK_DIR = Path().absolute()
 IMAGE_DIR = WORK_DIR / "src/dlim_api/images"
-
-mask_generator = None  # TODO CHANGE to object etc dict wahtever
-
-
-def get_masks(anns):  # TODO MOVE
-    if len(anns) == 0:
-        return
-
-    sorted_anns = sorted(anns, key=(lambda x: x["area"]), reverse=True)
-    img = np.ones((sorted_anns[0]["segmentation"].shape[0], sorted_anns[0]["segmentation"].shape[1], 4))
-    img[:, :, 3] = 0
-    for ann in sorted_anns:
-        m = ann["segmentation"]
-        color_mask = np.concatenate([np.random.random(3), [0.35]])
-        img[m] = color_mask
-    return img
 
 
 @sam_blueprint.route("/blueprint-hc", methods=["GET"])
@@ -54,15 +40,7 @@ def send_racoon():
 @sam_blueprint.route("/sam", methods=["PUT"])
 def load_sam():
     try:
-        WORK_DIR = Path().absolute()  # Gets current working dir
-        SAM_CHECKPOINT = WORK_DIR / "src/dlim_api/utils/model_checkpoints/sam_vit_h_4b8939.pth"
-        model_type = "vit_h"
-        device = "cuda"
-        sam = sam_model_registry[model_type](checkpoint=SAM_CHECKPOINT)
-        sam.to(device=device)
-        global mask_generator
-        mask_generator = SamAutomaticMaskGenerator(model=sam, points_per_batch=32)
-
+        sam_utils.load_sam()
         return "Successfuly loaded SAM"
     except Exception as e:
         return str(e)
@@ -70,18 +48,9 @@ def load_sam():
 
 @sam_blueprint.route("/sam", methods=["POST"])
 def segment_image():
-    image = request.files["image"]
-    image_name = image.filename
-    PATH_TO_IMG = IMAGE_DIR / image_name
-    image = Image.open(image.stream)
-
-    image = np.asarray(image)
-    masks = mask_generator.generate(image)
-    image = Image.fromarray(image)
-    image_masks = Image.fromarray(np.uint8(get_masks(masks) * 255))
-    image.paste(image_masks, (0, 0), image_masks)
-    image.save(PATH_TO_IMG)
-
     # img.save(PATH_TO_IMG)
-
-    return send_file(PATH_TO_IMG)
+    image = request.files["image"]
+    try:
+        return send_file(sam_utils.segment_image(image))
+    except Exception as e:
+        return str(e)
