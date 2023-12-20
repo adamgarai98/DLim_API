65111e83aec15cab5a51e174429b0c47a08de74e
Adam <a.garai98@gmail.com>
2023-12-20 23:55:34 +0000

    Cleanup (#18)
    * Logging
    
    * Delete sam.py
    
    * Logging
    
    * Multithreading achieved internally
    
    Needs a lot of cleaning up but multithreading is implemented in a basic form
    
    * Clean up and checking for CUDA

--- src/dlim_api/blueprints/sam_bp.py
+++ src/dlim_api/blueprints/sam_bp.py
@@ -1,29 +1,13 @@
 from __future__ import annotations
 
-import base64
 import concurrent.futures
 import logging
-import multiprocessing
 import os
 import threading
 import uuid
 from pathlib import Path
 
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-import torch
-from flask import (
-    Blueprint,
-    abort,
-    current_app,
-    jsonify,
-    make_response,
-    request,
-    send_file,
-)
-from PIL import Image
-from segment_anything import SamAutomaticMaskGenerator, SamPredictor, sam_model_registry
+from flask import Blueprint, jsonify, request, send_file
 from werkzeug.utils import secure_filename
 
 from dlim_api.utils.model_utils import sam_utils
@@ -41,39 +25,14 @@ executor = concurrent.futures.ThreadPoolExecutor()
 task_data = {}
 
 
-@sam_blueprint.route("/racoon-in-a-suit", methods=["GET"])
-def send_racoon():
-    # Gets current working dir
-    PATH_TO_RACOON = WORK_DIR / "src/dlim_api/images/racoon_in_suit.jpg"
-    return send_file(PATH_TO_RACOON, mimetype="image/jpeg")
-
-
 @sam_blueprint.route("/sam/load", methods=["PUT"])
 def load_sam():
     try:
         sam_utils.load_sam()
-        return "Successfuly loaded SAM"
+        return "Successfuly loaded SAM", 201
     except Exception as e:
         logger.error(str(e))
         return str(e)
-
-
-def run_segment_image(image_path, task_id):
-    global task_data
-    # task_id = str(uuid.uuid4())
-    task_data[task_id] = {}
-    current_task = task_data[task_id]
-    current_task["status"] = "running"
-
-    try:
-        logger.info("Starting segmentation")
-        image_path = sam_utils.segment_image(image_path)
-        logger.info("Finished segmentation")
-
-        current_task["status"] = "completed"
-        current_task["path_to_image"] = image_path
-    except Exception as e:
-        logger.error(str(e))
 
 
 @sam_blueprint.route("/sam/segment/", methods=["POST"])
@@ -91,49 +50,45 @@ def segment_image():
 
     # Save file first
     image_name = secure_filename(image.filename)
-    logger.info(str(image_name))
-    logger.info(str(image.mimetype))
+    logger.debug(str(image_name))
+    logger.debug(str(image.mimetype))
     PATH_TO_IMG = IMAGE_DIR / image_name
     image.save(PATH_TO_IMG)
 
-    # image_name = image.filename
-    # PATH_TO_IMG = IMAGE_DIR / image_name
-    # image.save(PATH_TO_IMG)
-
     # Submit task
-    future = executor.submit(run_segment_image, PATH_TO_IMG, task_id)
+    future = executor.submit(sam_utils.run_segment_image, PATH_TO_IMG, task_id)
     return jsonify({"message": "Request in progress", "task_id": task_id})
 
 
 @sam_blueprint.route("/sam/segment/status/<task_id>", methods=["GET"])
 def get_segementation_status(task_id):
     # Check the status of the task
-    # task_id = int(task_id)
     task = task_data.get(task_id, {"status": "not_found", "message": "Task not found"})
 
+    # If task is completed return image
     if task["status"] == "completed":
-        # If task is completed return result
-        logger.info("Sending image back")
+        logger.info("Sending image")
         return send_file(task["path_to_image"])
 
     return jsonify(task)
 
 
-@sam_blueprint.route("/sam/segment/image-only", methods=["POST"])
-def segment_image_only():
-    # Check if there is an image
-    if "image" not in request.files:
-        logger.info("No image provided")
-        return jsonify({"error": "No image provided"}), 400
+# TODO change below for a flag maybe
 
-    image = request.files["image"]
+# @sam_blueprint.route("/sam/segment/image-only", methods=["POST"])
+# def segment_image_only():
+#     # Check if there is an image
+#     if "image" not in request.files:
+#         logger.info("No image provided")
+#         return jsonify({"error": "No image provided"}), 400
 
-    try:
-        image_path = sam_utils.segment_image(image)
-        image_response = send_file(image_path)
-        os.remove(image_path)
-        logger.info(os.path.isfile(image_path))  # Seems to be deleting
-        return image_response
-    except Exception as e:
-        logger.error(str(e))
-        return str(e)
+#     image = request.files["image"]
+#     try:
+#         image_path = sam_utils.segment_image(image)
+#         image_response = send_file(image_path)
+#         os.remove(image_path)
+#         logger.info(os.path.isfile(image_path))  # Seems to be deleting
+#         return image_response
+#     except Exception as e:
+#         logger.error(str(e))
+#         return str(e)
--- src/dlim_api/utils/model_utils/sam_utils.py
+++ src/dlim_api/utils/model_utils/sam_utils.py
@@ -1,18 +1,12 @@
 from __future__ import annotations
 
 import logging
-import multiprocessing
-import os
-import uuid
 from pathlib import Path
 
-import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from flask import Blueprint, abort, current_app, jsonify, request, send_file
 from PIL import Image
-from segment_anything import SamAutomaticMaskGenerator, SamPredictor, sam_model_registry
+from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 
 logger = logging.getLogger(__name__)
 
@@ -20,13 +14,12 @@ WORK_DIR = Path().absolute()
 IMAGE_DIR = WORK_DIR / "src/dlim_api/images"
 
 
-mask_generator = None  # TODO CHANGE to object etc dict wahtever
+mask_generator = None  # TODO hmm
 
 
 def get_masks(anns):
     if len(anns) == 0:
         return
-
     sorted_anns = sorted(anns, key=(lambda x: x["area"]), reverse=True)
     img = np.ones((sorted_anns[0]["segmentation"].shape[0], sorted_anns[0]["segmentation"].shape[1], 4))
     img[:, :, 3] = 0
@@ -37,13 +30,14 @@ def get_masks(anns):
     return img
 
 
-def load_sam():  # TODO cuda or not
+def load_sam():
     global mask_generator
     try:
         WORK_DIR = Path().absolute()  # Gets current working dir
         SAM_CHECKPOINT = WORK_DIR / "src/dlim_api/utils/model_checkpoints/sam_vit_h_4b8939.pth"
         model_type = "vit_h"
-        device = "cuda"
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        logger.info(f"{device} detected")
         sam = sam_model_registry[model_type](checkpoint=SAM_CHECKPOINT)
         sam.to(device=device)
         mask_generator = SamAutomaticMaskGenerator(model=sam, points_per_batch=32)
@@ -55,20 +49,49 @@ def load_sam():  # TODO cuda or not
 
 def segment_image(image_path):
     try:
-        # image = open(image_path)
-
-        # image_name = image.filename
-        # PATH_TO_IMG = IMAGE_DIR / image_name
-
+        # Open image
         image = Image.open(image_path)
         image = np.asarray(image)
 
+        # Generate Masks
+        logger.info("Generating masks")
         masks = mask_generator.generate(image)
+        logger.info("Masks Generated")
 
+        # Paste masks over image
         image = Image.fromarray(image)
         image_masks = Image.fromarray(np.uint8(get_masks(masks) * 255))
         image.paste(image_masks, (0, 0), image_masks)
         image.save(image_path)
+
+        # Return pasted image
         return image_path
+
+    except torch.cuda.CudaOutOfMemory:
+        # Handle out-of-memory error
+        logger.error("Cuda out of memory")
+        # Add below to the dict
+        # result = {"status": "failed", "message": "CUDA out of memory error"}
+
+    except Exception as e:
+        # Handle other exceptions if needed
+        logger.error(str(e))
+        # Add below to the dict
+        # result = {"status": "failed", "message": f"Error: {str(e)}"}
+
+
+def run_segment_image(image_path, task_id):
+    global task_data
+    task_data[task_id] = {}
+    current_task = task_data[task_id]
+    current_task["status"] = "running"
+
+    try:
+        logger.info("Starting segmentation")
+        image_path = segment_image(image_path)
+        logger.info("Finished segmentation")
+
+        current_task["status"] = "completed"
+        current_task["path_to_image"] = image_path
     except Exception as e:
         logger.error(str(e))
